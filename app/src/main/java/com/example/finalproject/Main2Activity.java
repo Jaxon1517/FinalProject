bf5545dd7924a8d4fd8de7230f444c00f383a2b8
devauntea <devauntea.thompson@csedge.com>
2020-06-20 14:59:49 -0400

    skills and looks

--- app/src/main/java/com/example/finalproject/Main2Activity.java
+++ app/src/main/java/com/example/finalproject/Main2Activity.java
@@ -2,7 +2,9 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+    public void newspage(View view){
+        Intent intent = new Intent(this, forumPosts.class);
+        startActivity(intent);
     }
 }
