fe6225b842b067de5cd5e6ad9fa00f723a5537ab
devauntea <devauntea.thompson@csedge.com>
2020-06-24 13:22:59 -0400

    Merge branch 'master' of https://github.com/Jaxon1517/FinalProject
    # Conflicts:
    #	app/src/main/res/layout/activity_forum.xml

--- app/src/main/java/com/example/finalproject/Main2Activity.java
+++ app/src/main/java/com/example/finalproject/Main2Activity.java
@@ -6,15 +6,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class Main2Activity extends AppCompatActivity {
+
+    boolean clicked = false;
+    boolean clickedChat = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        final LottieAnimationView lottieClickNews = findViewById(R.id.lottieClickNews);
+        lottieClickNews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (clicked = true){
+                    Intent intent = new Intent( Main2Activity.this, forumPosts.class);
+                    startActivity(intent);
+                    clicked = true;
+                }
+            }
+        });
+
+        final LottieAnimationView lottieClickChat = findViewById(R.id.lottieClickChat);
+        lottieClickChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (clickedChat = true){
+                    Intent intent = new Intent( Main2Activity.this, MainActivity.class);
+                    startActivity(intent);
+                    clickedChat = true;
+                }
+            }
+        });
     }
-    public void newspage(View view){
-        Intent intent = new Intent(this, forumPosts.class);
-        startActivity(intent);
-    }
+
 }
