21defc29f107f186f5d1a388a46afa160c4ccb50
Jaxon1517 <James.hartley@csedge.org>
2020-06-26 13:21:56 -0400

    hi

--- app/src/main/java/com/example/finalproject/Forum.java
+++ app/src/main/java/com/example/finalproject/Forum.java
@@ -5,10 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -29,7 +34,32 @@ import java.util.ArrayList;
         ForumPostsAdapter = new ForumPostAdapter(this, Posts);
         forumView.setAdapter(ForumPostsAdapter);
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        databasePosts = database.getReference().child("Posts");
+
+        databasePosts.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Posts.add(dataSnapshot.getValue(forumPosts.class));
+                ForumPostsAdapter.notifyDataSetChanged();
+            }
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            }
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+            }
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+
     }
+
     public void floatButt(View view){
         Intent intent = new Intent( this, makePost.class);
         startActivity(intent);
