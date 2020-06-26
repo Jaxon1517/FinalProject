778d429b6c33a32f0b635d2b51886958dacc1dca
devauntea <devauntea.thompson@csedge.com>
2020-06-26 13:26:47 -0400

    finally done

--- app/src/main/java/com/example/finalproject/chat.java
+++ app/src/main/java/com/example/finalproject/chat.java
@@ -1,5 +1,6 @@
 package com.example.finalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -11,8 +12,12 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.annotations.Nullable;
 
 import java.util.ArrayList;
 
@@ -40,14 +45,39 @@ public class chat extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         databaseMessages = database.getReference().child("messages");
 
-        final LottieAnimationView lottieClickSent = findViewById(R.id.lottieClickSent);
+
+        databaseMessages.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                MessagesList.add(dataSnapshot.getValue(Message.class));
+                messagesAdapter.notifyDataSetChanged();
+            }
+                @Override
+                public void onChildChanged (@NonNull DataSnapshot dataSnapshot, @Nullable String s){
+                }
+                @Override
+                public void onChildRemoved (@NonNull DataSnapshot dataSnapshot){
+                }
+                @Override
+                public void onChildMoved (@NonNull DataSnapshot dataSnapshot, @Nullable String s){
+                }
+                @Override
+                public void onCancelled (@NonNull DatabaseError databaseError){
+                }
+            });
+
+
+
+
+            final LottieAnimationView lottieClickSent = findViewById(R.id.lottieClickSent);
         lottieClickSent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (clicked = true){
                     String message = editText.getText().toString();
-                    MessagesList.add(new com.example.finalproject.Message(message));
+                    //MessagesList.add(new com.example.finalproject.Message(message));
                     editText.setText("");
+
                     com.example.finalproject.Message chatMessage = new Message(message);
                     databaseMessages.push().setValue(chatMessage);
                     closeKeyboard();
